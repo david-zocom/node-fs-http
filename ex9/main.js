76afb13a125208d7fa7ab00d751450af129bc065
David Andersson <David Andersson>
2018-09-11 10:33:04 +0200

    Bugfix: create streams when needed instead of reusing

--- ex9/main.js
+++ ex9/main.js
@@ -12,13 +12,37 @@
 // Om URL är något annat så ska du skicka en annan sida, som visar felmeddelandet 404.
 
 
+// 9d* Template - uppdatera webbservern så att den byter ut alla förekomster av { } och försöker utvärdera dem, som Vue eller React. Eller hitta på din egen syntax! Tips: Node har en "chunk size" på 64kB så du behöver inte ta hänsyn till vad som händer nära "gränsen" på varje bit data. Exempel:
+// <p>Number: { 5 + 5 } </p> → <p>Number: 10 </p>
+
+
 const http = require('http');
 const fs = require('fs');
-let port = 3000;
+let port = 3001;
 const { Transform } = require('stream');
-const shoutingStream = new Transform({
+
+let createShoutingStream = () => new Transform({
 	transform(chunk, encoding, callback) {
 		this.push(chunk.toString().toUpperCase());
+		callback();
+	}
+});
+let createTemplateStream = () => new Transform({
+	transform(chunk, encoding, callback) {
+		let text = chunk.toString();
+		// substring och indexOf, (regex), split
+		// console.log('template: text is "'+text+'"');
+		let pieces = text.split('{');  // 'hej {user}!'
+		for( let i=1; i<pieces.length; i++ ) {
+			let bracketEnd = pieces[i].indexOf('}'); // 'user}!'
+			let expression = pieces[i].substring(0, bracketEnd);
+			let rest = pieces[i].substring(bracketEnd + 1);
+			pieces[i] = eval(expression) + rest;
+			console.log('piece #'+i, pieces[i]);
+		}
+		text = pieces.join('');
+		console.log('template: new text is "'+text+'"')
+		this.push(text);
 		callback();
 	}
 });
@@ -26,19 +50,20 @@ const shoutingStream = new Transform({
 let server = http.createServer();
 server.addListener('request', (request, response) => {
 	console.log(`Det kom ett request med url=${request.url} och method=${request.method}`);
-	if( request.url === '/' ) {
-		let readStream = fs.createReadStream('./index.html');
-		readStream.pipe(response);
-	}
-	else if(  request.url === '/upper/') {
-		let readStream = fs.createReadStream('./index.html');
-		readStream.pipe(shoutingStream).pipe(response);
-	}
-	else {
-		let readStream = fs.createReadStream('./404.html');
-		readStream.pipe(response);
-	}
+	let file = '404.html';
+	if( request.url === '/' || request.url === '/upper/' )
+		file = './index.html';
+	let stream = fs.createReadStream(file);
+	let shoutingStream = createShoutingStream();
+	let templateStream = createTemplateStream();
+	// stream = stream.pipe(shoutingStream);
+	if( request.url === '/' || request.url === '/upper/' )
+		stream = stream.pipe(templateStream);
+	if(  request.url === '/upper/')
+		stream = stream.pipe(shoutingStream);
+	stream.pipe(response);
 	// response.end();
+	// stream.pipe(shoutingStream).pipe(response);
 })
 server.listen(port);
 console.log('Server listening on port ' + port);
